853c884a46b77fdeee9a38438f899f329bd4beeb
Jamie Laptop <Jamiegaskin@gmail.com>
2014-10-04 13:05:39 -0700

    Finished html and css prototypes

---
+++ js/formvalid.js
@@ -0,0 +1,48 @@
+$(function () {
+  $('a.item').removeClass('active');
+  $(this).addClass('active');
+});
+var validationObj = {
+    firstName: {
+      identifier  : 'email',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your first name'
+        }
+      ]
+    },
+    lastName: {
+      identifier  : 'last-name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your last name'
+        }
+      ]
+    },
+    username: {
+      identifier : 'username',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a username'
+        }
+      ]
+    },
+    password: {
+      identifier : 'password',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  };
+$('.form').form(validationObj);
+});
